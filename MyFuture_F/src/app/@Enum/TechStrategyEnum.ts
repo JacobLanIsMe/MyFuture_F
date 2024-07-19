93026403c5b9980df539ddc188bee6f8907f0183
JacobLanIsMe <maimaisatt@gmail.com>
2024-07-19 09:26:53 +0800

    change the result after date changing

---
+++ MyFuture_F/src/app/@Enum/TechStrategyEnum.ts
@@ -0,0 +1,6 @@
+export enum TechStrategyEnum {
+    BullishPullback,
+    JumpEmpty,
+    Organized,
+    Sandwich
+}
--- MyFuture_F/src/app/index/strategy/strategy.component.ts
+++ MyFuture_F/src/app/index/strategy/strategy.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StrategyEnum } from 'src/app/@Enum/StrategyEnum';
+import { TechStrategyEnum } from 'src/app/@Enum/TechStrategyEnum';
 import { StockBaseModel } from 'src/app/@Model/stockBaseModel';
 import { GetStockService } from 'src/app/@Service/get-stock.service';
 
@@ -11,27 +12,52 @@ import { GetStockService } from 'src/app/@Service/get-stock.service';
 export class StrategyComponent {
   constructor(private getStockService: GetStockService) {
     this.selectedDate = new Date().toISOString().split('T')[0];
+    this.selectedStrategy = TechStrategyEnum.BullishPullback;
   }
   selectedDate: string;
+  selectedStrategy: TechStrategyEnum;
+  dateChanging() {
+    switch (this.selectedStrategy) {
+      case TechStrategyEnum.BullishPullback:
+        this.getBullishPullbackStocks();
+        break;
+      case TechStrategyEnum.JumpEmpty:
+        this.getJumpEmptyStocks();
+        break;
+      case TechStrategyEnum.Organized:
+        this.getOrganizedStocks();
+        break;
+      case TechStrategyEnum.Sandwich:
+        this.getSandwichStocks();
+        break;
+      default:
+        this.getBullishPullbackStocks();
+        break;
+    }
+  }
   getBullishPullbackStocks() {
+    this.selectedStrategy = TechStrategyEnum.BullishPullback;
     this.statusBeforeGetResult();
     this.getStockService.getBullishPullbackStocks(this.selectedDate).subscribe(res => {
       this.statusAfterGetResult(res);
     })
   }
   getJumpEmptyStocks() {
+    this.selectedStrategy = TechStrategyEnum.JumpEmpty;
     this.statusBeforeGetResult();
     this.getStockService.getJumpEmptyStocks(this.selectedDate).subscribe(res => {
       this.statusAfterGetResult(res);
     })
   }
   getOrganizedStocks() {
+    this.selectedStrategy = TechStrategyEnum.Organized;
     this.statusBeforeGetResult();
     this.getStockService.getOrganizedStocks(this.selectedDate).subscribe(res => {
       this.statusAfterGetResult(res);
     })
   }
   getSandwichStocks() {
+    this.selectedStrategy = TechStrategyEnum.Sandwich;
     this.statusBeforeGetResult();
     this.getStockService.getSandwichStocks(this.selectedDate).subscribe(res => {
       this.statusAfterGetResult(res);
